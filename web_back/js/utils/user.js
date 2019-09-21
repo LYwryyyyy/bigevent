1644606dae10cb792b389f46b2f3097d93fa3628
unknown <1014208056@qq.com>
2019-09-21 21:58:10 +0800

    初始化

---
+++ web_back/js/utils/user.js
@@ -0,0 +1,36 @@
+var user = {
+  /**
+   * 用户登陆
+   * @param {*} userName 用户名
+   * @param {*} password 密码
+   * @param {*} callBack 回调函数
+   */
+  login: function(userName, password, callBack) {
+    $.post(
+      URLIST.user_login,
+      {
+        user_name: userName,
+        password: password
+      },
+      function(res) {
+        // res就是响应体。是由$.post这个方法决定的。
+        callBack(res);
+      }
+    );
+  },
+  /**
+   * 用户退出
+   * @param {*} callBack
+   */
+  logout: function(callBack) {
+    $.post(URLIST.user_logout, function(res) {
+      callBack(res);
+    });
+  },
+
+  getInfo: function(callBack) {
+    $.get(URLIST.user_getInfo, function(res) {
+      callBack(res);
+    });
+  }
+};
